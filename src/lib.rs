e7c2b61325bcd3998fa5b109d11f8b00825dea34
kei67 <kei.02to.14@gmail.com>
2022-04-24 17:19:21 +0900

    I-I その2

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
+struct Doller{
+    amount:i32
+}
+
+impl Doller {
+    fn new(amount:i32) -> Self {
+        Doller{amount:amount}
+    }
+    fn times(&mut self, multipler:i32) -> Self{
+        Doller {amount:self.amount*multipler}
+    }
+}
+
+
 #[cfg(test)]
 mod mony_tests {
+    use super::*;
     #[test]
     fn it_works() {
-        let five: Doller = Doller::new(5);
-        five.times(2);
-        assert_eq!(10, five.amount);
+        let mut five: Doller = Doller::new(5);
+        let product: Doller = five.times(2);
+        assert_eq!(10, product.amount);
+        let product: Doller = five.times(3);
+        assert_eq!(15, product.amount);
+        
     }
 }
